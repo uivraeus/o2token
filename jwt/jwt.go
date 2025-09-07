cbd19935873bbd526ee6362faebbca6ebf7c7695
Ulf Ivraeus <ulf.ivraeus@gmail.com>
2025-09-07 05:46:01 +0000

    Add stdin (pipe) option to jwt tool

--- jwt/jwt.go
+++ jwt/jwt.go
@@ -1,23 +1,49 @@
 package main
 
 import (
+	"bufio"
 	"flag"
 	"fmt"
+	"io"
 	"os"
+	"strings"
 
 	h "o2token/helpers"
 )
+
+const usageMsg = `Usage: jwt [optional flags] <jwt-string>
+   or: echo <jwt-string> | jwt [optional flags]`
 
 func main() {
 	pureOutputPtr := flag.Bool("pure", false, "show decoded JWT body without any re-formatting or annotations (default false)")
 
 	flag.Parse()
-	if flag.NArg() != 1 {
-		fmt.Fprint(os.Stderr, "Usage: jwt [optional flags] <jwt-string>\n")
+
+	var jwtStr string
+
+	stat, _ := os.Stdin.Stat()
+	isPipe := (stat.Mode() & os.ModeCharDevice) == 0
+
+	if isPipe && flag.NArg() > 0 {
+		fmt.Fprintf(os.Stderr, "Error: Cannot accept both piped input and command line argument\n%s\n", usageMsg)
 		os.Exit(1)
 	}
 
-	jwtStr := flag.Arg(0)
+	if isPipe {
+		reader := bufio.NewReader(os.Stdin)
+		input, err := io.ReadAll(reader)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "Error reading from stdin: %v\n", err)
+			os.Exit(1)
+		}
+		jwtStr = strings.TrimSpace(string(input))
+	} else if flag.NArg() == 1 {
+		jwtStr = flag.Arg(0)
+	} else {
+		fmt.Fprintln(os.Stderr, usageMsg)
+		os.Exit(1)
+	}
+
 	if *pureOutputPtr {
 		fmt.Println(h.JwtToString(jwtStr))
 	} else {
